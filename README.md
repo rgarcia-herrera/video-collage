4f41e6fe40ced837fee1d77510b366dba52fa725
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2016-07-17 18:57:02 -0500

    use python-vlc for playback instead of cvlc
    this way it doesn't skip as much. Better readme.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # video-collage
-watch random fragments of a video collection
+
+Watch random fragments of a video collection.
+
+You might call it thusly:
+
+    python video_collage.py --path /mnt --lapse_min=30 --lapse_max=100 2>/dev/null
+
+It will go through you'r video collection in /mnt and play at random
+order lapses of 30 to 100 seconds, at random. In this example I added
+2>/dev/null to ignore stuff stderr from libvlc. It will print to
+stdout played videos.
--- video_collage.py
+++ video_collage.py
@@ -1,11 +1,11 @@
 import argparse
-from sh import find, cvlc
-from random import shuffle, randint
+from sh import find
+from random import shuffle, randrange, random
 import vlc
+from time import sleep
 
 parser = argparse.ArgumentParser(description='play random samples from films')
 parser.add_argument('--path', type=str)
-parser.add_argument('--min_start', type=int, default=3*60, help="minimum start time in seconds")
 parser.add_argument('--lapse_min', type=int, default=3*60, help="minimum lapse length in seconds")
 parser.add_argument('--lapse_max', type=int, default=11*60, help="maximum lapse length in seconds")
 args = parser.parse_args()
@@ -26,17 +26,15 @@ for f in filmes:
 shuffle(filtered)
 
 vlci = vlc.Instance()
+p = vlci.media_player_new()
 
 for f in filtered:
-    media = vlci.media_new(f)
-    media.parse()
-    media_length = media.get_duration()/1000
-
-    lapse = randint(args.lapse_min, args.lapse_max)
-    start = randint(args.min_start, media_length - lapse)
-    stop  = start + lapse
-    
-    cvlc(f,
-         no_osd=True,
-         start_time=start,
-         stop_time=stop )
+    try:
+        media = vlci.media_new(f)
+        p.set_media(media)
+        p.play()
+        p.set_position(random())
+        print f
+        sleep(randrange(args.lapse_min, args.lapse_max))
+    except:
+        p.stop()
