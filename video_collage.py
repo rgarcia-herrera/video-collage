6c68510511670fdf8127bfa76c4a7627538fac07
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2016-07-17 18:25:00 -0500

    no magic numbers, default parameters in argparse, no random times outside of media length

--- video_collage.py
+++ video_collage.py
@@ -1,17 +1,20 @@
 import argparse
 from sh import find, cvlc
 from random import shuffle, randint
+import vlc
 
 parser = argparse.ArgumentParser(description='play random samples from films')
-parser.add_argument('root_path', type=str)
+parser.add_argument('--path', type=str)
+parser.add_argument('--min_start', type=int, default=3*60, help="minimum start time in seconds")
+parser.add_argument('--lapse_min', type=int, default=3*60, help="minimum lapse length in seconds")
+parser.add_argument('--lapse_max', type=int, default=11*60, help="maximum lapse length in seconds")
 args = parser.parse_args()
 
 
-filmes = find( args.root_path, '-iname', '*mp4',
+filmes = find( args.path, '-iname', '*mp4',
                '-o', '-iname', '*avi',
                '-o', '-iname', '*mkv',               
                '-o', '-iname', '*mov')
-
 
 filtered = []
 
@@ -22,13 +25,17 @@ for f in filmes:
 
 shuffle(filtered)
 
-# 90 minutes, 60 seconds each
-start = randint(3*60, 90*60)
-lapse = 7
-lapse = randint(3*60, 11*60)
-stop  = start + lapse
+vlci = vlc.Instance()
 
 for f in filtered:
+    media = vlci.media_new(f)
+    media.parse()
+    media_length = media.get_duration()/1000
+
+    lapse = randint(args.lapse_min, args.lapse_max)
+    start = randint(args.min_start, media_length - lapse)
+    stop  = start + lapse
+    
     cvlc(f,
          no_osd=True,
          start_time=start,
