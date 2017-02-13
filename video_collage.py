6633331bfc50525752a3b1ee7d88e711ef315e6c
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2017-02-12 20:35:14 -0600

    can read several paths

--- video_collage.py
+++ video_collage.py
@@ -5,31 +5,31 @@ import vlc
 from time import sleep
 
 parser = argparse.ArgumentParser(description='play random samples from films')
-parser.add_argument('--path', type=str)
+parser.add_argument('--path', type=str, nargs="+")
 parser.add_argument('--lapse_min', type=int, default=3*60, help="minimum lapse length in seconds")
 parser.add_argument('--lapse_max', type=int, default=11*60, help="maximum lapse length in seconds")
 args = parser.parse_args()
 
-
-filmes = find( args.path, '-iname', '*mp4',
-               '-o', '-iname', '*avi',
-               '-o', '-iname', '*mkv',
-               '-o', '-iname', '*iso',
-               '-o', '-iname', '*vob',
-               '-o', '-iname', '*ogm',
-               '-o', '-iname', '*m4v',
-               '-o', '-iname', '*ogg',
-               '-o', '-iname', '*flv',
-               '-o', '-iname', '*divx',
-               '-o', '-iname', '*wmv',
-               '-o', '-iname', '*mov')
-
 filtered = []
 
-for f in filmes:
-    film = f.strip()
-    if film.lower().find('sample') == -1:
-        filtered.append( film )
+for path in args.path:
+    filmes = find(path, '-iname', '*mp4',
+                  '-o', '-iname', '*avi',
+                  '-o', '-iname', '*mkv',
+                  '-o', '-iname', '*iso',
+                  '-o', '-iname', '*vob',
+                  '-o', '-iname', '*ogm',
+                  '-o', '-iname', '*m4v',
+                  '-o', '-iname', '*ogg',
+                  '-o', '-iname', '*flv',
+                  '-o', '-iname', '*divx',
+                  '-o', '-iname', '*wmv',
+                  '-o', '-iname', '*mov')
+
+    for f in filmes:
+        film = f.strip()
+        if film.lower().find('sample') == -1:
+            filtered.append( film )
 
 shuffle(filtered)
 
